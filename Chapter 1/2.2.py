46c387e52b4ab50105c90cdea45af7880fc0ed4a
Jagriti-17 <157792591+Jagriti-17@users.noreply.github.com>
2024-02-19 19:23:41 +0530

    Create 2.2.py

---
+++ Chapter 1/2.2.py
@@ -0,0 +1,8 @@
+# Start the Python interpreter and use it as a calculator. 
+
+# How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile. 
+
+# Solution:
+
+miles=10/1.61
+print(miles)
