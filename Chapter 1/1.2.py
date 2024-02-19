ecbc464beae8bff5e2a3f865eb5fd683bb9cf682
Jagriti-17 <157792591+Jagriti-17@users.noreply.github.com>
2024-02-19 20:02:18 +0530

    Create 1.2.py

--- Chapter 1/1.2.py
+++ Chapter 1/1.2.py
@@ -1,5 +1,4 @@
-# 
-If you are trying to print a string, what happens if you leave out one of the quotation marks, or both? 
+# If you are trying to print a string, what happens if you leave out one of the quotation marks, or both? 
 
 # Solution:
 # If we try to print a string without one quotation marks we will get a syntax error because it will be assuming the word is a variable .
