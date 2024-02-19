f61d410218118270f15cf42d4bc21e2a5242ab21
Jagriti-17 <157792591+Jagriti-17@users.noreply.github.com>
2024-02-19 19:06:11 +0530

    Create 1-1.5.py

---
+++ Chapter 1/1-1.5.py
@@ -0,0 +1,4 @@
+# What happens if you have two values with no operator between them?
+
+# Solution:
+# If we try to add two values with no operator between them then it will result in syntax error as it doesn’t know what to do.
