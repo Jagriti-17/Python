e6b35732c16fc6b55b583c0f920a65850edae0eb
Jagriti-17 <157792591+Jagriti-17@users.noreply.github.com>
2024-02-19 20:04:32 +0530

    Update 1.5.py

--- Chapter 1/1.5.py
+++ Chapter 1/1.5.py
@@ -1,4 +1,5 @@
-# What happens if you have two values with no operator between them?
+# 
+What happens if you have two values with no operator between them?
 
 # Solution:
 # If we try to add two values with no operator between them then it will result in syntax error as it doesn’t know what to do.
