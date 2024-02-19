e31523dbd4f207a914f96df705884b7f592b7bf5
Jagriti-17 <157792591+Jagriti-17@users.noreply.github.com>
2024-02-19 19:48:01 +0530

    Update 1.4.py

--- Chapter 1/1.4.py
+++ Chapter 1/1.4.py
@@ -1,5 +1,4 @@
-#
-In math notation, leading zeros are okay, as in 02. What happens if you try this in Python? 
+#In math notation, leading zeros are okay, as in 02. What happens if you try this in Python? 
 
 # Solution:
 # If we try to add leading zeros we will get syntax error because leading zeros in decimal integer literals are not permitted. Python does not allow the use of leading zeros.
