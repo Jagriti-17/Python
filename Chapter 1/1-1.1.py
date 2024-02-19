795db9d6b6dc5a7408fe930bcb2f59efb51466a0
Jagriti-17 <157792591+Jagriti-17@users.noreply.github.com>
2024-02-19 18:59:43 +0530

    Update 1-1.1.py

--- Chapter 1/1-1.1.py
+++ Chapter 1/1-1.1.py
@@ -1,1 +1,3 @@
+# In a print statement, what happens if you leave out one of the parentheses, or both?
 
+In a print statement , when we leave out one of the parentheses it will raise a syntax error because it reads one and fails to get the completing one.
