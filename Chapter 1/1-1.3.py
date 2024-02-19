5d7443527318c2bfaa9fbc65b85909b746f5d955
Jagriti-17 <157792591+Jagriti-17@users.noreply.github.com>
2024-02-19 19:06:59 +0530

    Update 1-1.3.py

--- Chapter 1/1-1.3.py
+++ Chapter 1/1-1.3.py
@@ -1,4 +1,4 @@
-#You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2?
+# You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2?
 
 # Solution:
-#If we add a plus sign before a number does not change its value. In the expression 2++2, the two plus signs next to each other will be interpreted as a positive number followed by another positive number. 
+# If we add a plus sign before a number does not change its value. In the expression 2++2, the two plus signs next to each other will be interpreted as a positive number followed by another positive number. 
